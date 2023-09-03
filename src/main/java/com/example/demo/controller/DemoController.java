9bd11590ac7458edd03b31ff02456664a16b33fa
DoanNamTruc <doannamtruc0123@gmail.com>
2023-09-03 14:05:30 +0700

    update missing file

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1/demo")
+public class DemoController {
+
+    ResponseEntity hello() {
+        return ResponseEntity.status(HttpStatus.OK).body("Hello demo user");
+    }
+}
